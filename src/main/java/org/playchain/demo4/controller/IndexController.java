55943739bd620775455c43521dd905ddd9980bc8
liujinlong <liujinlong@jiudaotech.com>
2017-08-21 19:14:07 +0800

    OAuth 2.0授权认证服务资源服务器

---
+++ src/main/java/org/playchain/demo4/controller/IndexController.java
@@ -0,0 +1,21 @@
+package org.playchain.demo4.controller;
+
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * Created by Liu on 2017/8/21.
+ */
+@RestController
+@EnableResourceServer
+public class IndexController {
+
+    @RequestMapping("/tokencall")
+    public String secureCall() {
+        return "success (id: " + UUID.randomUUID().toString().toUpperCase() + ")";
+    }
+}
