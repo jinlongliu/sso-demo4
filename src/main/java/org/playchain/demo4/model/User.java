192963387892a094897a1be99e4622fbbd894440
liujinlong <liujinlong@jiudaotech.com>
2017-08-25 14:21:17 +0800

    测试数据模型

--- src/main/java/org/playchain/demo4/model/User.java
+++ src/main/java/org/playchain/demo4/model/User.java
@@ -9,12 +9,18 @@ public class User implements Serializable {
 
     private String last_name;
 
+    private String username;
+
+    private String password;
+
     private static final long serialVersionUID = 1L;
 
-    public User(Integer id, String first_name, String last_name) {
+    public User(Integer id, String first_name, String last_name, String username, String password) {
         this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
+        this.username = username;
+        this.password = password;
     }
 
     public User() {
@@ -45,6 +51,22 @@ public class User implements Serializable {
         this.last_name = last_name == null ? null : last_name.trim();
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
     @Override
     public boolean equals(Object that) {
         if (this == that) {
@@ -59,7 +81,9 @@ public class User implements Serializable {
         User other = (User) that;
         return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
             && (this.getFirst_name() == null ? other.getFirst_name() == null : this.getFirst_name().equals(other.getFirst_name()))
-            && (this.getLast_name() == null ? other.getLast_name() == null : this.getLast_name().equals(other.getLast_name()));
+            && (this.getLast_name() == null ? other.getLast_name() == null : this.getLast_name().equals(other.getLast_name()))
+            && (this.getUsername() == null ? other.getUsername() == null : this.getUsername().equals(other.getUsername()))
+            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()));
     }
 
     @Override
@@ -69,6 +93,8 @@ public class User implements Serializable {
         result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
         result = prime * result + ((getFirst_name() == null) ? 0 : getFirst_name().hashCode());
         result = prime * result + ((getLast_name() == null) ? 0 : getLast_name().hashCode());
+        result = prime * result + ((getUsername() == null) ? 0 : getUsername().hashCode());
+        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
         return result;
     }
 
@@ -81,6 +107,8 @@ public class User implements Serializable {
         sb.append(", id=").append(id);
         sb.append(", first_name=").append(first_name);
         sb.append(", last_name=").append(last_name);
+        sb.append(", username=").append(username);
+        sb.append(", password=").append(password);
         sb.append(", serialVersionUID=").append(serialVersionUID);
         sb.append("]");
         return sb.toString();
